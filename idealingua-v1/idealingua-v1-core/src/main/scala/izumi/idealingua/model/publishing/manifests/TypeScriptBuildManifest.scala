f1770eaa499d39c59de036022c5d799c537659ac
Denys Andriushchenko <34319172+dandriushchenko@users.noreply.github.com>
2020-11-03 13:37:26 +0200

    Typescript & dependencies (#141)
    * feat: setting new dependency versions and moving to peers
    
    * feat: respect on build side
    
    * fix: ts version
    
    * revert: getting back peer dependencies to dev section as from npm@7 it will be installed btw
    
    * fix: type definition errors (upgrade from 10 to 14 LTS NodeJS)
    
    * up

--- idealingua-v1/idealingua-v1-core/src/main/scala/izumi/idealingua/model/publishing/manifests/TypeScriptBuildManifest.scala
+++ idealingua-v1/idealingua-v1-core/src/main/scala/izumi/idealingua/model/publishing/manifests/TypeScriptBuildManifest.scala
@@ -24,12 +24,12 @@ object YarnOptions {
   def example: YarnOptions = YarnOptions(
     projectNaming = ProjectNamingRule.example,
     dependencies = List(
-      ManifestDependency("moment", "^2.20.1"),
-      ManifestDependency("@types/node", "^10.7.1"),
-      ManifestDependency("@types/websocket", "0.0.39"),
+      ManifestDependency("moment", "^2.29.1"),
+      ManifestDependency("@types/node", "^14.14.6"),
+      ManifestDependency("@types/websocket", "1.0.1"),
     ),
     devDependencies = List(
-      ManifestDependency("typescript", "3.2.2"),
+      ManifestDependency("typescript", "4.0.5"),
     ),
     scope = "@TestScope",
   )
