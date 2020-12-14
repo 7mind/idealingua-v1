98d88b597c41f7c55a6578cf31678c33a86fe045
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2020-12-15 00:06:37 +0100

    Update sbt-pgp to 2.0.2 (#156)

--- project/project/PluginVersions.scala
+++ project/project/PluginVersions.scala
@@ -2,7 +2,7 @@ object PV {
   val izumi = "1.0.0-M1"
 
   val sbt_scoverage = "1.6.1"
-  val sbt_pgp = "2.0.1"
+  val sbt_pgp = "2.0.2"
   val sbt_assembly = "0.15.0"
 
   val scala_js_version = "1.2.0"
