aaa2d17a40d854fa5976a8f3270cb0663eb173ef
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2021-01-05 22:23:10 +0100

    Update async-http-client to 2.12.2 (#169)

--- project/Versions.scala
+++ project/Versions.scala
@@ -13,7 +13,7 @@ object V {
 
   // java-only dependencies below
   // java, we need it bcs http4s ws client isn't ready yet
-  val asynchttpclient = "2.12.1"
+  val asynchttpclient = "2.12.2"
 
   val slf4j = "1.7.30"
   val typesafe_config = "1.4.1"
