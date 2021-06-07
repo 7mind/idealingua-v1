b3c78ed9f04e5134043c511148dfcaee2b70228f
Scala Steward <me@scala-steward.org>
2021-06-07 12:04:44 +0200

    Update sbt-assembly to 1.0.0

--- project/project/PluginVersions.scala
+++ project/project/PluginVersions.scala
@@ -3,7 +3,7 @@ object PV {
 
   val sbt_scoverage = "1.8.2"
   val sbt_pgp = "2.1.1"
-  val sbt_assembly = "0.15.0"
+  val sbt_assembly = "1.0.0"
 
   val scala_js_version = "1.5.1"
   val crossproject_version = "1.0.0"
