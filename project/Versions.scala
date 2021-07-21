d4e3c6ed45adc94492f33c0e0cfa4e6dd6c5255f
Scala Steward <me@scala-steward.org>
2021-07-21 06:08:07 +0200

    Update scala-xml to 2.0.1

--- project/Versions.scala
+++ project/Versions.scala
@@ -7,7 +7,7 @@ object V {
   val scalameta = "4.4.24"
   val fastparse = "2.3.2"
 
-  val scala_xml = "2.0.0"
+  val scala_xml = "2.0.1"
 
   // java-only dependencies below
   // java, we need it bcs http4s ws client isn't ready yet
