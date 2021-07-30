853b230c50ea688eeb69b01d469f3912b0ea3709
Kai <450507+neko-kai@users.noreply.github.com>
2021-07-30 12:01:10 +0100

    Update to sbtgen-0.0.78, Scala.js 1.6.0 (#257)
    Take Scala.js dependency values from upstream sbtgen

--- project/project/PluginVersions.scala
+++ project/project/PluginVersions.scala
@@ -4,8 +4,4 @@ object PV {
   val sbt_scoverage = "1.8.2"
   val sbt_pgp = "2.1.1"
   val sbt_assembly = "1.0.0"
-
-  val scala_js_version = "1.5.1"
-  val crossproject_version = "1.0.0"
-  val scalajs_bundler_version = "0.20.0"
 }
