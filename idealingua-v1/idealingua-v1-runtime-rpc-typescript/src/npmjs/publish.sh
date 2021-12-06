93a5e7db4c8cfbfbb0aba7e69bdb5c421ee76d13
Denys Andriushchenko <dandriushchenko@playq.net>
2021-12-06 18:02:13 +0200

    name refactoring

--- idealingua-v1/idealingua-v1-runtime-rpc-typescript/src/npmjs/publish.sh
+++ idealingua-v1/idealingua-v1-runtime-rpc-typescript/src/npmjs/publish.sh
@@ -4,8 +4,9 @@ set -ex
 
 export THISDIR="$( cd "$(dirname "$0")" ; pwd -P )"
 
-pkgFile=package.json
+pkgFile='package.json'
 pkgName='@izumi-framework/izumi-runtime-typescript'
+pkgPath='dist'
 
 pushd .
 cd $THISDIR
@@ -16,13 +17,13 @@ npm install
 tsc -p ./tsconfig.json
 tsc -p ./tsconfig.es.json
 
-node -p "JSON.stringify({...require('./package.json'), name: '${pkgName}'}, null, 2)" > dist/package.json
-node -p "JSON.stringify({...require('./package.json'), name: '${pkgName}-es'}, null, 2)" > dist-es/package.json
+node -p "JSON.stringify({...require('./package.json'), name: '${pkgName}'}, null, 2)" > ${pkgPath}/package.json
+node -p "JSON.stringify({...require('./package.json'), name: '${pkgName}-es'}, null, 2)" > ${pkgPath}-es/package.json
 
 npm install json
 ./node_modules/json/lib/json.js -I -f dist/package.json -e "this.version=\"${IZUMI_VERSION}\""
 
-( cd dist && npm publish --access public || exit 1 )
-( cd dist-es && npm publish --access public || exit 1 )
+( cd ${pkgPath} && npm publish --access public || exit 1 )
+( cd ${pkgPath}-es && npm publish --access public || exit 1 )
 
 popd
