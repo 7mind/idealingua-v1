c6535c7f92b4baef02b3e6b87feb73fec47a02b9
Denys Andriushchenko <dandriushchenko@playq.net>
2021-12-06 17:50:06 +0200

    publish both es5&6 versions

--- idealingua-v1/idealingua-v1-runtime-rpc-typescript/src/npmjs/publish.sh
+++ idealingua-v1/idealingua-v1-runtime-rpc-typescript/src/npmjs/publish.sh
@@ -4,17 +4,25 @@ set -ex
 
 export THISDIR="$( cd "$(dirname "$0")" ; pwd -P )"
 
+pkgFile=package.json
+pkgName='@izumi-framework/izumi-runtime-typescript'
+
 pushd .
 cd $THISDIR
 
 cp -R ../main/resources/runtime/typescript/irt .
 npm install
-tsc
-cp package.json dist/
+
+tsc -p ./tsconfig.json
+tsc -p ./tsconfig.es.json
+
+node -p "JSON.stringify({...require('./package.json'), name: '${pkgName}'}, null, 2)" > dist/package.json
+node -p "JSON.stringify({...require('./package.json'), name: '${pkgName}-es'}, null, 2)" > dist-es/package.json
+
 npm install json
 ./node_modules/json/lib/json.js -I -f dist/package.json -e "this.version=\"${IZUMI_VERSION}\""
-cd dist
-npm publish --access public || exit 1
+
+( cd dist && npm publish --access public || exit 1 )
+( cd dist-es && npm publish --access public || exit 1 )
 
 popd
-
