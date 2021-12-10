d2b7b3b4c281708ccde8c29c0cc3c67b44dd25e6
Denys Andriushchenko <34319172+dandriushchenko@users.noreply.github.com>
2021-12-10 15:06:06 +0200

    ES Modules 5 & 6 (#300)
    * stable ts
    
    * shebang
    
    * separating ts config for es5&6 versions
    
    * moving back translator version
    
    * publish both es5&6 versions
    
    * git ignore
    
    * fix: ts version
    
    * fix: initial pkg name
    
    * name refactoring
    
    * fix: default package name
    
    * fix: ts version

--- idealingua-v1/idealingua-v1-runtime-rpc-typescript/src/npmjs/publish.sh
+++ idealingua-v1/idealingua-v1-runtime-rpc-typescript/src/npmjs/publish.sh
@@ -1,18 +1,29 @@
-#!/bin/bash -xe
+#!/usr/bin/env bash
+
+set -ex
 
 export THISDIR="$( cd "$(dirname "$0")" ; pwd -P )"
+
+pkgFile='package.json'
+pkgName=$(cat package.json | node -pe 'JSON.parse(fs.readFileSync(0)).name')
+pkgPath='dist'
 
 pushd .
 cd $THISDIR
 
 cp -R ../main/resources/runtime/typescript/irt .
 npm install
-tsc
+
+tsc -p ./tsconfig.json
+tsc -p ./tsconfig.es.json
+
 cp package.json dist/
+node -p "JSON.stringify({...require('./package.json'), name: '${pkgName}-es'}, null, 2)" > ${pkgPath}-es/package.json
+
 npm install json
 ./node_modules/json/lib/json.js -I -f dist/package.json -e "this.version=\"${IZUMI_VERSION}\""
-cd dist
-npm publish --access public || exit 1
+
+( cd ${pkgPath} && npm publish --access public || exit 1 )
+( cd ${pkgPath}-es && npm publish --access public || exit 1 )
 
 popd
-
