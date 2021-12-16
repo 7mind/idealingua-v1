ba8babddae85fcd0f544b001894880215b2ffd7c
Denys Andriushchenko <dandriushchenko@playq.net>
2021-12-16 11:38:53 +0200

    added es-modules packages versioning

--- idealingua-v1/idealingua-v1-runtime-rpc-typescript/src/npmjs/publish.sh
+++ idealingua-v1/idealingua-v1-runtime-rpc-typescript/src/npmjs/publish.sh
@@ -17,11 +17,12 @@ npm install
 tsc -p ./tsconfig.json
 tsc -p ./tsconfig.es.json
 
-cp package.json dist/
+cp package.json ${pkgPath}/
 node -p "JSON.stringify({...require(path.resolve(__dirname, 'package.json')), name: '${pkgName}-es'}, null, 2)" > ${pkgPath}-es/package.json
 
 npm install json
-./node_modules/json/lib/json.js -I -f dist/package.json -e "this.version=\"${IZUMI_VERSION}\""
+./node_modules/json/lib/json.js -I -f ${pkgPath}/package.json -e "this.version=\"${IZUMI_VERSION}\""
+./node_modules/json/lib/json.js -I -f ${pkgPath}-es/package.json -e "this.version=\"${IZUMI_VERSION}\""
 
 ( cd ${pkgPath} && npm publish --access public || exit 1 )
 ( cd ${pkgPath}-es && npm publish --access public || exit 1 )
