e5ceff4178c6f2fa235c0588818f7ad57a4dd55c
Scala Steward <scala_steward@virtuslab.com>
2023-03-14 12:56:16 +0000

    Update http4s-blaze-client, ... to 0.23.14

--- project/Versions.scala
+++ project/Versions.scala
@@ -4,7 +4,7 @@ object V {
 
   val scalatest = "3.2.15"
   val http4s = "0.23.18"
-  val http4s_blaze = "0.23.13"
+  val http4s_blaze = "0.23.14"
 
   val scalameta = "4.5.13"
   val fastparse = "2.3.3"
