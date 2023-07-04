7d760edfed2481a8fbf466d9994272d7f3057bc8
Kai <450507+neko-kai@users.noreply.github.com>
2023-07-04 12:36:11 +0100

    Update sbtgen to 0.0.99

--- project/Versions.scala
+++ project/Versions.scala
@@ -11,16 +11,17 @@ object V {
 
   val scala_xml = "2.1.0"
 
+  val kind_projector = "0.13.2"
+
+  val circe_derivation = "0.13.0-M5"
+  val circe_generic_extras = "0.14.3"
+
+  val scala_java_time = "2.5.0"
+
   // java-only dependencies below
   // java, we need it bcs http4s ws client isn't ready yet
   val asynchttpclient = "2.12.3"
 
   val slf4j = "1.7.30"
   val typesafe_config = "1.4.2"
-
-  val kind_projector = "0.13.2"
-
-  val circe_generic_extras = "0.14.3"
-
-  val scala_java_time = "2.5.0"
 }
