36e1735f94feee962f4ffb104223660baebcd563
Scala Steward <scala_steward@virtuslab.com>
2023-09-06 12:38:10 +0000

    Update fundamentals-bio, ... to 1.1.0

--- project/project/PluginVersions.scala
+++ project/project/PluginVersions.scala
@@ -1,7 +1,7 @@
 object PV {
-  val izumi = "1.1.0-M25"
+  val izumi = "1.1.0"
 
   val sbt_scoverage = "2.0.8"
-  val sbt_pgp = "2.1.1"
-  val sbt_assembly = "1.1.1"
+  val sbt_pgp       = "2.1.1"
+  val sbt_assembly  = "1.1.1"
 }
