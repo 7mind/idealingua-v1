6fd1d54f18ed2778e4e197d21ac8ec54bd1989c8
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2023-10-17 16:23:08 +0200

    Update scalameta to 4.8.12 (#464)

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,19 +1,18 @@
-
 object V {
   // foundation
 
-  val scalatest = "3.2.16"
-  val http4s = "0.23.23"
+  val scalatest    = "3.2.16"
+  val http4s       = "0.23.23"
   val http4s_blaze = "0.23.15"
 
-  val scalameta = "4.8.7" // Not available for Scala 3 yet
+  val scalameta = "4.8.12" // Not available for Scala 3 yet
   val fastparse = "3.0.2" // 3.0.0 is available for Scala 3
 
   val scala_xml = "2.2.0"
 
   val kind_projector = "0.13.2"
 
-  val circe_derivation = "0.13.0-M5"
+  val circe_derivation     = "0.13.0-M5"
   val circe_generic_extras = "0.14.3"
 
   val scala_java_time = "2.5.0"
@@ -22,6 +21,6 @@ object V {
   // java, we need it bcs http4s ws client isn't ready yet
   val asynchttpclient = "2.12.3"
 
-  val slf4j = "1.7.30"
+  val slf4j           = "1.7.30"
   val typesafe_config = "1.4.2"
 }
