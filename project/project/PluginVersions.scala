72d92871abf8b0f1bd528f9ab1adc6db87b2cd40
Scala Steward <scala_steward@virtuslab.com>
2023-10-28 12:03:43 +0000

    Update sbt-assembly to 2.1.4

--- project/project/PluginVersions.scala
+++ project/project/PluginVersions.scala
@@ -2,6 +2,6 @@ object PV {
   val izumi = "1.1.0-M25"
 
   val sbt_scoverage = "2.0.8"
-  val sbt_pgp = "2.1.1"
-  val sbt_assembly = "1.1.1"
+  val sbt_pgp       = "2.1.1"
+  val sbt_assembly  = "2.1.4"
 }
