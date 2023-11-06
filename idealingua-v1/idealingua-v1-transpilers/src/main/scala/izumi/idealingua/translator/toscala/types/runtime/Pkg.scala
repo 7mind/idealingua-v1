7e3d2ccb4de2cba04cdfc108e5eff094a60aea39
Kai <450507+neko-kai@users.noreply.github.com>
2023-11-06 16:11:32 +0100

    fix -Xsource:3 error

--- idealingua-v1/idealingua-v1-transpilers/src/main/scala/izumi/idealingua/translator/toscala/types/runtime/Pkg.scala
+++ idealingua-v1/idealingua-v1-transpilers/src/main/scala/izumi/idealingua/translator/toscala/types/runtime/Pkg.scala
@@ -3,8 +3,10 @@ package izumi.idealingua.translator.toscala.types.runtime
 import izumi.idealingua.model.common.DomainId
 import izumi.idealingua.translator.toscala.types.ScalaTypeConverter
 
+import scala.annotation.nowarn
 import scala.reflect.{ClassTag, classTag}
 
+@nowarn("msg=constructor modifiers are assumed by synthetic .* method")
 final case class Pkg private (pkgParts: Seq[String]) {
   final val conv = new ScalaTypeConverter(DomainId(pkgParts.init, pkgParts.last))
 
