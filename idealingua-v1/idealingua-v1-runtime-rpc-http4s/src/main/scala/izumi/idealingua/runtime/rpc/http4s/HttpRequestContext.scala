d4a3c70ec6098939d92bb9058c51b7c3b0be12df
Alex Liubymov <arowcap@gmail.com>
2023-12-15 16:58:16 +0200

    remove HttpRequestContext

--- idealingua-v1/idealingua-v1-runtime-rpc-http4s/src/main/scala/izumi/idealingua/runtime/rpc/http4s/HttpRequestContext.scala
+++
@@ -1,6 +0,0 @@
-package izumi.idealingua.runtime.rpc.http4s
-
-import org.http4s.Request
-
-// we can't make it a case class, see https://github.com/scala/bug/issues/11239
-class HttpRequestContext[F[_]](val request: Request[F], val context: Any)
