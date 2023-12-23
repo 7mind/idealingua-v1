981d508ddf7cea5a496697c974e71ae0be0f2127
Alex Liubymov <arowcap@gmail.com>
2023-12-23 15:23:33 +0200

    add names to the IRT contexts

--- idealingua-v1/idealingua-v1-runtime-rpc-http4s/src/main/scala/izumi/idealingua/runtime/rpc/http4s/IRTContextServices.scala
+++ idealingua-v1/idealingua-v1-runtime-rpc-http4s/src/main/scala/izumi/idealingua/runtime/rpc/http4s/IRTContextServices.scala
@@ -3,8 +3,10 @@ package izumi.idealingua.runtime.rpc.http4s
 import izumi.functional.bio.{IO2, Monad2}
 import izumi.idealingua.runtime.rpc.http4s.ws.WsContextSessions
 import izumi.idealingua.runtime.rpc.{IRTServerMiddleware, IRTServerMultiplexor}
+import izumi.reflect.Tag
 
 trait IRTContextServices[F[+_, +_], AuthCtx, RequestCtx, WsCtx] {
+  def name: String
   def authenticator: IRTAuthenticator[F, AuthCtx, RequestCtx]
   def serverMuxer: IRTServerMultiplexor[F, RequestCtx]
   def middlewares: Set[IRTServerMiddleware[F, RequestCtx]]
@@ -30,18 +32,21 @@ trait IRTContextServices[F[+_, +_], AuthCtx, RequestCtx, WsCtx] {
 
 object IRTContextServices {
   type AnyContext[F[+_, +_], AuthCtx] = IRTContextServices[F, AuthCtx, ?, ?]
+  type AnyWsContext[F[+_, +_], AuthCtx, RequestCtx] = IRTContextServices[F, AuthCtx, RequestCtx, ?]
 
-  def apply[F[+_, +_], AuthCtx, RequestCtx, WsCtx](
+  def apply[F[+_, +_], AuthCtx, RequestCtx: Tag, WsCtx: Tag](
     authenticator: IRTAuthenticator[F, AuthCtx, RequestCtx],
     serverMuxer: IRTServerMultiplexor[F, RequestCtx],
     middlewares: Set[IRTServerMiddleware[F, RequestCtx]],
     wsSessions: WsContextSessions[F, RequestCtx, WsCtx],
   ): Default[F, AuthCtx, RequestCtx, WsCtx] = Default(authenticator, serverMuxer, middlewares, wsSessions)
 
-  final case class Default[F[+_, +_], AuthCtx, RequestCtx, WsCtx](
+  final case class Default[F[+_, +_], AuthCtx, RequestCtx: Tag, WsCtx: Tag](
     authenticator: IRTAuthenticator[F, AuthCtx, RequestCtx],
     serverMuxer: IRTServerMultiplexor[F, RequestCtx],
     middlewares: Set[IRTServerMiddleware[F, RequestCtx]],
     wsSessions: WsContextSessions[F, RequestCtx, WsCtx],
-  ) extends IRTContextServices[F, AuthCtx, RequestCtx, WsCtx]
+  ) extends IRTContextServices[F, AuthCtx, RequestCtx, WsCtx] {
+    override def name: String = s"${Tag[RequestCtx].tag}:${Tag[WsCtx].tag}"
+  }
 }
