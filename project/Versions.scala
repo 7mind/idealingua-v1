e5d75e1f4f13525cbc4cf31ba0b8cfd84e5214ec
Scala Steward <scala_steward@virtuslab.com>
2024-06-16 12:57:26 +0000

    Update scalameta to 4.9.6

--- project/Versions.scala
+++ project/Versions.scala
@@ -6,7 +6,7 @@ object V {
   val http4s       = "0.23.27"
   val http4s_blaze = "0.23.16"
 
-  val scalameta = "4.9.5" // Not available for Scala 3 yet
+  val scalameta = "4.9.6" // Not available for Scala 3 yet
   val fastparse = "3.1.0" // 3.0.0 is available for Scala 3
 
   val scala_xml = "2.3.0"
