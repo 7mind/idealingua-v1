001e16558fd9aeafec14b38b2624a192d427bed5
Pavel Shirshov <pshirshov@eml.cc>
2024-10-26 22:44:08 +0100

    wip: nixify

--- devops/.validate-publishing.sh
+++ devops/.validate-publishing.sh
@@ -3,5 +3,5 @@
 set -e
 set -x
 
-[[ "$CI_PULL_REQUEST" != "false"  ]] && echo "Publishing not allowed on P/Rs" && exit 0
+[[ "$CI_PULL_REQUEST" == "true"  ]] && echo "Publishing not allowed on P/Rs" && exit 0
 [[ ! ("$CI_BRANCH" == "develop" || "$CI_BRANCH_TAG" =~ ^v.*$ ) ]] && echo "Publishing not allowed (CI_BRANCH=$CI_BRANCH, CI_BRANCH_TAG=$CI_BRANCH_TAG)" && exit 0
