870d468b43a469bf9ff52fbf5c4bff5d02cd243e
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2024-11-05 19:14:42 +0100

    Update http4s-blaze-client, ... to 0.23.17 (#564)

--- project/Versions.scala
+++ project/Versions.scala
@@ -4,7 +4,7 @@ object V {
   val scalatest = "3.2.19"
 
   val http4s       = "0.23.28"
-  val http4s_blaze = "0.23.16"
+  val http4s_blaze = "0.23.17"
 
   val scalameta = "4.10.2" // Not available for Scala 3 yet
   val fastparse = "3.1.1" // 3.0.0 is available for Scala 3
