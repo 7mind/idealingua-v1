58769f898106fcb2fa73ed549f92c57233f1639d
Pavel Shirshov <pshirshov@eml.cc>
2024-11-28 18:36:41 +0000

    flake.nix provides derivation

---
+++ refresh.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -xe
+
+do_update() {
+    escaped="${2//[\/&]/\\&}"
+
+    sed -i -E \
+        -e "s/version\s*=\s*\"([a-z0-9-]\.?)+\";/version = \"$1\";/g" \
+        -e "s/depsSha256\s*=\s*\"([^\"])*\";/depsSha256 = \"$escaped\";/g" \
+        $3
+}
+
+
+do_update "0.0.0" "" ./flake.nix
+
+IDEALINGUA_VERSION=$(cat version.sbt | sed -r 's/.*\"(.*)\".**/\1/' | sed -E "s/-SNAPSHOT//")
+
+PROPER_HASH=$(nix build --print-build-logs 2>&1 . | grep "got:" | awk '{print $2}')
+
+do_update "$IDEALINGUA_VERSION" "$PROPER_HASH" ./flake.nix
