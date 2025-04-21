e7b99a9544ab3f516ba0795f1c5bd0f2b0c37ac7
Pavel Shirshov <pshirshov@eml.cc>
2025-04-21 17:56:42 +0100

    wip

--- .mobala/steps/run-test-scala.sh
+++ .mobala/steps/run-test-scala.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
-set -xeuo pipefail
+set -euo pipefail
 
 function run-test-scala() {
-  unset _JAVA_OPTIONS
   shellspec --format documentation --jobs "${NUMCPU}" -o junit --reportdir ./target/spec-reports/scala ./.mobala/steps/spec/scala_spec.sh
 }
