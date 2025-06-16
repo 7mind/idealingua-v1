942d4b662dd71e79385da531159928016c31fce6
Kai <450507+neko-kai@users.noreply.github.com>
2025-06-16 18:11:13 +0100

    Setting version to 1.4.4

--- flake.nix
+++ flake.nix
@@ -23,7 +23,7 @@
         packages = rec {
           idealingua-v1 = sbt.lib.mkSbtDerivation {
             pkgs = pkgs;
-            version = "1.4.3";
+            version = "1.4.4";
             pname = "idealingua-v1";
             src = ./.;
             depsSha256 = "sha256-O2dWncCsinkAiJ369a14/eRoS/gjpTdRSn/hBQNNS5A=";
