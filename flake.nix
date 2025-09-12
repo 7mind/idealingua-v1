118cd2e38f3cde807d9630b03a27e4f9dd5dd465
Kai <450507+neko-kai@users.noreply.github.com>
2025-09-12 20:36:40 +0100

    Setting version to 1.4.5

--- flake.nix
+++ flake.nix
@@ -23,10 +23,10 @@
         packages = rec {
           idealingua-v1 = sbt.lib.mkSbtDerivation {
             pkgs = pkgs;
-            version = "1.4.4";
+            version = "1.4.5";
             pname = "idealingua-v1";
             src = ./.;
-            depsSha256 = "sha256-O2dWncCsinkAiJ369a14/eRoS/gjpTdRSn/hBQNNS5A=";
+            depsSha256 = "sha256-D3ae+8So1xbuA3R06aAOU6qGqOl0s104Yczg1PO7+xk=";
             nativeBuildInputs = with pkgs; [
               coursier
               libarchive
