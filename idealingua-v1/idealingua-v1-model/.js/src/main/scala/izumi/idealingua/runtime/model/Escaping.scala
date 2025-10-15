5fc17bb1f5c1de5db4ec14374997fec36778f27c
ntyshchenko-playq <ntyshchenko@playq.net>
2025-10-15 16:02:06 +0300

    Use encodeURIComponent instead of encodeURI

--- idealingua-v1/idealingua-v1-model/.js/src/main/scala/izumi/idealingua/runtime/model/Escaping.scala
+++ idealingua-v1/idealingua-v1-model/.js/src/main/scala/izumi/idealingua/runtime/model/Escaping.scala
@@ -4,7 +4,7 @@ import scala.scalajs.js.URIUtils
 
 object Escaping {
   // TODO: we may need to use a better escaping
-  def escape(s: String): String = URIUtils.encodeURI(s)
+  def escape(s: String): String = URIUtils.encodeURIComponent(s)
 
-  def unescape(s: String): String = URIUtils.decodeURI(s)
+  def unescape(s: String): String = URIUtils.encodeURIComponent(s)
 }
