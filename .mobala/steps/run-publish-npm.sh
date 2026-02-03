0f03c294aec6c9cbb6850a117cf790f1903deea3
Kai <450507+neko-kai@users.noreply.github.com>
2026-02-03 20:35:04 +0000

    ci: remove npm token again

--- .mobala/steps/run-publish-npm.sh
+++ .mobala/steps/run-publish-npm.sh
@@ -6,10 +6,5 @@ set -euo pipefail
 function run-publish-npm() {
   validate_publishing || exit 0
 
-  [[ -z "$TOKEN_NPM" ]] && echo "Missing TOKEN_NPM" && exit 0
-
-  echo "//registry.npmjs.org/:_authToken=${TOKEN_NPM}" > ~/.npmrc
-  npm whoami
-
   ./idealingua-v1/idealingua-v1-runtime-rpc-typescript/src/npmjs/publish.sh || exit 1
 }
