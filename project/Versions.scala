1e79665fad59bc6d688cd0ff89cbe2b738feb1f4
Scala Steward <scala_steward@virtuslab.com>
2026-02-04 16:00:21 +0000

    Update http4s-circe to 0.23.33

--- project/Versions.scala
+++ project/Versions.scala
@@ -3,7 +3,7 @@ object V {
 
   val scalatest = "3.2.19"
 
-  val http4s       = "0.23.30"
+  val http4s       = "0.23.33"
   val http4s_blaze = "0.23.17"
 
   val scalameta = "4.13.7" // Not available for Scala 3 yet
