027fd27dc8d919b784193b695a9520941029a206
Kai <450507+neko-kai@users.noreply.github.com>
2026-04-17 00:43:56 +0100

    Update izumi to 1.3.0-M1 (#608)
    Update Scala 3 to 3.8.3
    Update Scala.js to 1.21.0

--- project/project/PluginVersions.scala
+++ project/project/PluginVersions.scala
@@ -1,11 +1,11 @@
 object PV {
-  val izumi    = "1.2.24"
+  val izumi    = "1.3.0-M1"
   val packager = "1.11.7"
 
   val sbt_scoverage = "2.4.4"
   val sbt_pgp       = "2.3.1"
 
-  val scala_js_version = "1.20.2"
+  val scala_js_version = "1.21.0"
 }
 
 object PluginVersions {
